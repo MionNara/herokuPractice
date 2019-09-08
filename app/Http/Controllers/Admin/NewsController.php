3e9a14921bfd8612d496b252d21b9010f2bdbb0f
Mion Nara <rorschachmn@gmail.com>
2019-09-08 07:21:06 +0000

    first commit

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,5 +7,10 @@ use App\Http\Controllers\Controller;
 
 class NewsController extends Controller
 {
-    //
+    // 以下を追記 NewsControllerにaddというactionを実装
+    public function add ( )
+    {
+        return view('admin.news.create');
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,18 @@
 
 Route::get('/', function () {
     return view('welcome');
+    
+    //グループ化することで、何度もadmin/news...,admin/Profile...と書かなくてすむ
+    Route::group(['prefix' => 'admin'],function(){
+        Route::get('news/create','Admin\NewsController@add');
+        Route::get('profile/create','Admin\ProfileController@add');
+        Route::get('profile/edit','Admin\ProfileController@edit');
+    });
+    
+/*http://XXXXXX.jp/XXX というアクセスが来たときに、 
+AAAControllerのbbbというAction に渡すRoutingの設定    
+*/
+    Route::get('XXX','AAAController@bbb');
+
 });
+
