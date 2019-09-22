4e9a978350cc77e6b4bd47462dacb801b8ea2e69
Mion Nara <rorschachmn@gmail.com>
2019-09-22 07:28:59 +0000

    PHP10

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,25 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+ return view('welcome');
+});
+
+    /*
+    グループ化することで、何度もadmin/news...,admin/Profile...と書かなくてすむ
+    何か変更が会った時も、最初のprefixの部分だけ変更すればOK
+    */
     
-    //グループ化することで、何度もadmin/news...,admin/Profile...と書かなくてすむ
-    Route::group(['prefix' => 'admin'],function(){
-        Route::get('news/create','Admin\NewsController@add');
-        Route::get('profile/create','Admin\ProfileController@add');
-        Route::get('profile/edit','Admin\ProfileController@edit');
-    });
+Route::group(['prefix' => 'admin'],function(){
+    Route::get('news/create','Admin\NewsController@add');
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+});
+    
     
 /*http://XXXXXX.jp/XXX というアクセスが来たときに、 
 AAAControllerのbbbというAction に渡すRoutingの設定    
 */
     Route::get('XXX','AAAController@bbb');
 
-});
+
 
