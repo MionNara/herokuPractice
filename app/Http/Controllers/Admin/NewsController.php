8e43ac1083c06e101b36a0c679da227e6e9ec008
Mion Nara <rorschachmn@gmail.com>
2019-10-13 15:00:02 +0000

    PHP14

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+//PHP14 以下を追記　News Modelが扱えるようになる
+use App\News;
+
+
 class NewsController extends Controller
 {
     // 以下を追記 NewsControllerにaddというactionを実装
@@ -13,9 +17,34 @@ class NewsController extends Controller
         return view('admin.news.create');
     }
     
-    //PHP13 以下を追記
+    //PHP13 以下を追記　createアクション
     public function create(Request $request)
     {
+        
+        //PHP14 以下を追記
+        // Varidationを行う
+        $this->validate($request,News::$rules);
+        
+        $news = new News;
+        $form = $request->all();
+        
+        //フォームから画像が送信されてきたら保存して$news->image_pathに画像のパスを保存する
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        } else {
+            $news->image_path = null;
+        }
+        
+        //フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        //フォームから送信されてきたimageを削除する
+        unset($form['image']);
+        
+        //データベースに保存
+        $news->fill($form);
+        $news->save();
+        
         // admin/news/createへリダイレクト
         return redirect('admin/news/create');
     }
---
+++ app/News.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    //追記
+    protected $quarded = array('id');
+    
+    public static $rules = array(
+        'title' => 'required',
+        'body' => 'required',
+        );
+    
+    //Valitationでデータが異常であることを見つけた時には、データを保存せずに入力フォームへ戻す
+}
